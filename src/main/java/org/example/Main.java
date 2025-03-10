1d3ee22d823d97e7ec6e91711053fd9b372640d0
Begüm Neval Yılmaz <begumnevalylmz@gmail.com>
2025-03-10 19:25:15 +0300

    kilogram fiyatları eklendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,30 +5,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Ürünlerin kilogram fiyatları
+        double armut=2.14;
+        double elma=3.67;
+        double domates=1.11;
+        double muz=0.95;
+        double patlican=5;
+
         //Kullanıcıdan kaç kilo armut aldığı bilgisini isteyen kod bloğu
         System.out.println("Armut kaç kilo?: ");
-        double armut = scanner.nextDouble();
+        double armutAmount = scanner.nextDouble();
 
         //Kullanıcıdan kaç kilo elma aldığı bilgisini isteyen kod bloğu
         System.out.println("Elma kaç kilo?: ");
-        double elma = scanner.nextDouble();
+        double elmaAmount = scanner.nextDouble();
 
         //Kullanıcıdan kaç kilo domates aldığı bilgisini isteyen kod bloğu
         System.out.println("Domates kaç kilo?: ");
-        double domates = scanner.nextDouble();
+        double domatesAmount = scanner.nextDouble();
 
         //Kullanıcıdan kaç kilo muz aldığı bilgisini isteyen kod bloğu
         System.out.println("Muz kaç kilo?: ");
-        double muz = scanner.nextDouble();
+        double muzAmount = scanner.nextDouble();
 
         //Kullanıcıdan kaç kilo patlıcan aldığı bilgisini isteyen kod bloğu
         System.out.println("Patlıcan kaç kilo?: ");
-        double patlican = scanner.nextDouble();
+        double patlicanAmount = scanner.nextDouble();
 
         //Tutar hesabı yapan ve yazdıran kod bloğu
-        double sum = armut*2.14 + elma*3.67 + domates*1.11 + muz*0.95 + patlican*5 ;
-        System.out.println("Toplam Tutar: " + sum + "TL");
-
+        double sum = armutAmount*armut + elmaAmount*elma + domatesAmount*domates + muzAmount*muz + patlicanAmount*patlican ;
+        System.out.println("Toplam Tutar: " + sum + " TL");
 
     }
 }
