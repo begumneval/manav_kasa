9bf579d6053c21c00279a7eaf126bb7e8609c0f7
Begüm Neval Yılmaz <begumnevalylmz@gmail.com>
2025-03-10 19:17:47 +0300

    yorum satırları eklendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,32 @@
 package org.example;
-
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Kullanıcıdan kaç kilo armut aldığı bilgisini isteyen kod bloğu
         System.out.println("Armut kaç kilo?: ");
         double armut = scanner.nextDouble();
 
+        //Kullanıcıdan kaç kilo elma aldığı bilgisini isteyen kod bloğu
         System.out.println("Elma kaç kilo?: ");
         double elma = scanner.nextDouble();
 
+        //Kullanıcıdan kaç kilo domates aldığı bilgisini isteyen kod bloğu
         System.out.println("Domates kaç kilo?: ");
         double domates = scanner.nextDouble();
 
+        //Kullanıcıdan kaç kilo muz aldığı bilgisini isteyen kod bloğu
         System.out.println("Muz kaç kilo?: ");
         double muz = scanner.nextDouble();
 
+        //Kullanıcıdan kaç kilo patlıcan aldığı bilgisini isteyen kod bloğu
         System.out.println("Patlıcan kaç kilo?: ");
         double patlican = scanner.nextDouble();
 
+        //Tutar hesabı yapan ve yazdıran kod bloğu
         double sum = armut*2.14 + elma*3.67 + domates*1.11 + muz*0.95 + patlican*5 ;
-
         System.out.println("Toplam Tutar: " + sum + "TL");
 
 
